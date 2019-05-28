f413968550f16687d72e34044a7adaa45424971c
Kirill Tovpinec <kirilltovpinec@KirillTovpinets-Macbook-Air.local>
2019-05-28 12:28:32 +0300

    fix: fix server file

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,18 @@
-var app = require('http').createServer();
+const handleRequest = (req, res) => {
+	if (req.url === '/') {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	}
+}
+
+
+var app = require('http').createServer(handleRequest);
 var io = module.exports.io = require('socket.io')(app);
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const socketManager = require('./SocketManager');
 
 io.on('connection', socketManager);
-
 app.listen(PORT, () => {
 	console.log('Connect at port ' + PORT);
 })
