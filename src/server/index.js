8f59390e0515e07055f1311dfa61a1860226a44a
Kirill Tovpinec <kirilltovpinec@KirillTovpinets-Macbook-Air.local>
2019-05-29 11:28:20 +0300

    fix: fix server settings

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,14 @@
-var io = module.exports.io = require('socket.io')(app);
 const path = require('path');
 const fs = require('fs');
 
 var app = require('http').createServer((req, res) => {
 	if (req.url === '/') {
-		var filePath = path.join(__dirname, 'build', 'index.html'); 
+		var filePath = path.join(__dirname, '../../build', 'index.html'); 
 		var stream = fs.createReadStream(filePath);
 		stream.pipe(res);
 	}
 });
-
+var io = module.exports.io = require('socket.io')(app);
 const PORT = process.env.PORT || 3001;
 const socketManager = require('./SocketManager');
 
